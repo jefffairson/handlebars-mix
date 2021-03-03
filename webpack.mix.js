bf92ce2cb7915396b255b554042edc24b59b4c5f
jeFFF @ Tisseur de toile <jft@tisseur-de-toile.fr>
2021-03-03 16:26:58 +0100

    Configure browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,19 @@
 let mix = require('laravel-mix');
 
 require('laravel-mix-handlebars');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 mix
-  .js('src/assets/js/app.js', 'public/assets/js')
+  .webpackConfig(() => {
+    return {
+      plugins: [
+        new BrowserSyncPlugin({
+          watch: true,
+          server: { baseDir: ['public'] }
+        }),
+      ]
+    };
+  })
   .handlebars('src/', 'public/')
+  .js('src/assets/js/app.js', 'public/assets/js')
   .setPublicPath('public');
